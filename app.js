e08835e2baeb3d9e8f3923bb0df958502044c95c
Pratikshatippe <54614091+Pratikshatippe@users.noreply.github.com>
2019-09-09 00:17:14 +0530

    updated file

--- app.js
+++ app.js
@@ -1,5 +1,45 @@
-$(document).ready(function() { 
-    $(".red").click(function(){
-        $("ball-1").css('background', 'red');
-    })
-});
+// $(document).ready(function() { 
+//     $(".red").click(function(){
+//         $("ball-1").css('background', 'red');
+//     })
+// });
+function changeRed(){
+    document.getElementById('ball-1').style.backgroundColor="red";
+    document.getElementById('ball-2').style.backgroundColor="red";
+    document.getElementById('ball-3').style.backgroundColor="red";
+}
+function changeGreen(){
+    document.getElementById('ball-1').style.backgroundColor="green";
+    document.getElementById('ball-2').style.backgroundColor="green";
+    document.getElementById('ball-3').style.backgroundColor="green";
+}
+function changeBlack(){
+    document.getElementById('ball-1').style.backgroundColor="black";
+    document.getElementById('ball-2').style.backgroundColor="black";
+    document.getElementById('ball-3').style.backgroundColor="black";
+}
+function changeYellow(){
+    document.getElementById('ball-1').style.backgroundColor="yellow";
+    document.getElementById('ball-2').style.backgroundColor="yellow";
+    document.getElementById('ball-3').style.backgroundColor="yellow";
+}
+function changePurple(){
+    document.getElementById('ball-1').style.backgroundColor="purple";
+    document.getElementById('ball-2').style.backgroundColor="purple";
+    document.getElementById('ball-3').style.backgroundColor="purple";
+}
+function changeOrange(){
+    document.getElementById('ball-1').style.backgroundColor="orange";
+    document.getElementById('ball-2').style.backgroundColor="orange";
+    document.getElementById('ball-3').style.backgroundColor="orange";
+}
+function changePink(){
+    document.getElementById('ball-1').style.backgroundColor="pink";
+    document.getElementById('ball-2').style.backgroundColor="pink";
+    document.getElementById('ball-3').style.backgroundColor="pink";
+}
+function changeWhite(){
+    document.getElementById('ball-1').style.backgroundColor="white";
+    document.getElementById('ball-2').style.backgroundColor="white";
+    document.getElementById('ball-3').style.backgroundColor="white";
+}
